80083422b86780790e862d964e24455026370b02
Rahul <Rahul.Jaiswal@bentley.com>
2025-08-03 16:44:41 +0530

    routing

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
-import { Outlet, useParams } from "react-router-dom"
+import { Outlet, useParams, useNavigate, useSearchParams } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 export const Home = () => {
     return <div>
@@ -7,13 +9,18 @@ export const Home = () => {
     </div>
 }
 
-export const Default = () =>{
+export const Default = () => {
     return <div>Default</div>
 }
 
-export const ParamsComponent = () =>{
-    const {id} = useParams();
-    return <div>{`ParamsComponent ${id}`}</div>
+export const ParamsComponent = () => {
+    const { id } = useParams();
+    const dispatch = useDispatch()
+    const emails = useSelector((state) => state.emails);
+    const navigate = useNavigate();
+    const [URLSearchParams, SetURLSearchParams] = useSearchParams();
+
+    return <div>{`ParamsComponent ${id} ${URLSearchParams.get('key')}`}</div>
 }
 
-export const NotFound= () => <div>Not found</div>
+export const NotFound = () => <div>Not found</div>
